a951793c2b470f0b8a30ae1810038cffba3e8a08
Damián Silvani <munshkr@gmail.com>
2017-02-16 22:50:29 -0300

    VolcaBass mixin

---
+++ lib/xi/midi/volca_bass.rb
@@ -0,0 +1,22 @@
+module Xi::MIDI
+  module VolcaBass
+    CC = {
+      slide: 5,
+      expression: 11,
+      octave: 40,
+      lforate: 41,
+      lfoint: 42,
+      pitch1: 43,
+      pitch2: 44,
+      pitch3: 45,
+      attack: 46,
+      decay: 47,
+      cutoffegint: 48,
+      bgate: 49
+    }
+
+    def cc_parameters
+      CC
+    end
+  end
+end
