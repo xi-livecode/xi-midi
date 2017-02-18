7b30281a4e6e93acd4c6e3afeb42b83a04187e5e
Damián Silvani <munshkr@gmail.com>
2017-02-17 22:12:27 -0300

    Use the same version for other xi gems deps

--- test/xi/midi_test.rb
+++ test/xi/midi_test.rb
@@ -4,8 +4,4 @@ class Xi::MIDITest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Xi::MIDI::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
